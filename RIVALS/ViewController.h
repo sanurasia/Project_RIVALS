db62aff7695b1a33414b66f95c3572bf2a539588
Sanjay Chaurasia <sanjay.csjm@gmail.com>
2016-03-02 12:13:42 +0530

    Initial Commit

---
+++ RIVALS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RIVALS
+//
+//  Created by Sanjay Kumar on 02/03/16.
+//  Copyright © 2016 Sanjay Kumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
